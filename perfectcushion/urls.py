755bad1058e0ba30f0cde855fb0c0f276e5c611d
Ronald <ronhon01@gmail.com>
2019-06-26 08:59:13 -0400

    'first'

---
+++ perfectcushion/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from shop import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('shop/', include('shop.urls')),
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
